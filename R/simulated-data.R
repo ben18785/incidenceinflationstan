c573ed85f58c67a8a18d2202e60fe81b48d0e2e7
ben18785 <ben.c.lambert@gmail.com>
2021-05-03 00:03:42 +0100

    Added functionality to generate case series trajectories

--- R/simulated-data.R
+++ R/simulated-data.R
@@ -1,37 +1,104 @@
 #' Probability case remains undetected over time
 #'
 #' This is effectively a survival function, which shows the
-#' probability that an actual case originating on day t0
-#' remains unreported at t1.
+#' probability that an actual case originating on day t_onset
+#' remains unreported at t_1.
 #'
-#' @param t1 time subsequent to time when case originates, t1>=t0
-#' @param t0 time when case originates
-#' @param delay_mean mean of gamma distribution governing reporting delay distribution
-#' @param delay_sd std. dev. of gamma distribution governing reporting delay distribution
+#' @param t_1 observation time subsequent to time when case originates: t1>=t0
+#' @param t_onset time when case originates
+#' @param delay_parameters named list of 'mean' and 'sd' of gamma distribution
+#' characterising the reporting delay distribution
 #'
 #' @return a probability
-undetected_prob <- function(t1, t0, delay_mean, delay_sd){
-  1 - stats::pgamma(t1 - t0, delay_mean^2 / delay_sd^2,
+#' @examples
+#' incidenceinflation:::undetected_prob(2, 1, list(mean=10, sd=5))
+undetected_prob <- function(t_1, t_onset, delay_parameters){
+  if(t_1 < t_onset)
+    stop("t_1 must equal or exceed t_onset")
+  delay_mean <- delay_parameters$mean
+  delay_sd <- delay_parameters$sd
+  1 - stats::pgamma(t_1 - t_onset, delay_mean^2 / delay_sd^2,
                     delay_mean / delay_sd^2)
 }
 
 #' Probability a case is detected by a later time given it was unobserved
 #' earlier
 #'
-#' This is the probability that a case originating on day d0 is detected between
-#' days d1 and d2 given that it was unobserved on day d1. This probability is
-#' calculated using the survival functions (which indicate the probability a
-#' case remains undetected by a certain time):
-#' \deqn{prob(detect) = \frac{S(d1|d0) - S(d2|d0)}{S(d1|d0)}}
+#' This is the probability that a case originating on day day_onset is detected
+#' between days day_1 and day_2 given that it was unobserved on day day_1. This
+#' probability is calculated using the survival functions (which indicate the
+#' probability a case remains undetected by a certain time):
+#' \deqn{prob(detect) = \frac{S(day_1|day_onset) - S(day_2|day_onset)}{S(day_1|day_onset)}}
 #'
-#' @param d2 day of last observation d2 > d1 >= d0
-#' @param d1 day of first observation d1 >= d0
-#' @param d0 day when case originates
+#' @param day_2 day of last observation: day_2 > day_1 >= day_onset
+#' @param day_1 day of first observation: day_1 >= day_onset
+#' @param day_onset day when case originates
 #' @inheritParams undetected_prob
 #'
 #' @return a probability
-detected_after_unobserved_prob <- function(d2, d1, d0, delay_mean, delay_sd){
-  (undetected_prob(d1 + 0.5, d0, delay_mean, delay_sd) -
-   undetected_prob(d2 + 0.5, d0, delay_mean, delay_sd)) /
-  undetected_prob(d1 + 0.5, d0, delay_mean, delay_sd)
+detected_after_unobserved_prob <- function(day_2, day_1, day_onset,
+                                           delay_parameters){
+  if(day_2 < day_1)
+    stop("second observation date must be at same time or after first")
+  (undetected_prob(day_1 + 0.5, day_onset, delay_parameters) -
+   undetected_prob(day_2 + 0.5, day_onset, delay_parameters)) /
+  undetected_prob(day_1 + 0.5, day_onset, delay_parameters)
 }
+
+#' Cases arising on a given day which are reported between two subsequent days
+#'
+#' The number of cases originating on day_onset which are subsequently reported
+#' between day_1 and day_2 is modelled as a binomial distribution:
+#' \deqn{I_reported\sim binomial(I_true-I_observed, p_detect)}
+#' where I_observed is the number of cases originating on day_onset reported on
+#' day_1; p_detect is the probability a case was undetected on day_1 but
+#' is detected by day_2.
+#'
+#' @inheritParams detected_after_unobserved_prob
+#' @param I_true true case count originating on day_onset
+#' @param I_observed observed case count for cases originating on day_onset on
+#' day_1
+#'
+#' @return a count representing number of cases reported between day_1 and day_2
+observed_cases_single <- function(I_observed, I_true, day_2, day_1, day_onset,
+                                  delay_parameters){
+  if(I_true < I_observed)
+    stop("true case count must exceed reported")
+  I_remaining <- I_true - I_observed
+  stats::rbinom(1, I_remaining,
+                detected_after_unobserved_prob(day_2, day_1, day_onset,
+                                               delay_parameters))
+}
+
+
+#' Generate trajectory of reported cases for a given count originating on a
+#' particular day
+#'
+#' @inheritParams observed_cases_single
+#' @param days_reporting a vector of days on which cases were counted
+#'
+#' @return a vector of reported case counts of same length as days_reporting
+observed_cases_trajectory <- function(I_true, days_reporting, day_onset,
+                                      delay_parameters){
+
+  I_observed <- vector(length = length(days_reporting))
+  I_observed[1] <- 0
+  for(i in 1:length(days_reporting)){
+    if(i == 1) {
+      I_previous_obs <- 0
+      day_previous_report <- 0
+    } else {
+      I_previous_obs <- I_observed[i - 1]
+      day_previous_report <- days_reporting[i - 1]
+    }
+    new_cases <- observed_cases_single(I_previous_obs, I_true,
+                                       days_reporting[i],
+                                       day_previous_report,
+                                       day_onset,
+                                       delay_parameters)
+    I_observed[i] <- I_previous_obs + new_cases
+  }
+  I_observed
+}
+
+
--- tests/testthat/test-simulated-data.R
+++ tests/testthat/test-simulated-data.R
@@ -1,32 +1,101 @@
 test_that("undetected_prob returns correct probability", {
-  expect_true(abs(undetected_prob(20, 10, 10, 5)
+  expect_true(abs(undetected_prob(20, 10, list(mean=10, sd=5))
                   - 0.4334701) < 0.01)
 })
 
 test_that("undetected_prob approaches 0 as t1 approaches inf", {
-  expect_equal(undetected_prob(Inf, 10, 10, 5), 0)
+  expect_equal(undetected_prob(Inf, 10, list(mean=10, sd=5)), 0)
 })
 
 test_that("undetected_prob only relative time differences matter", {
-  expect_equal(undetected_prob(20, 10, 10, 5),
-               undetected_prob(30, 20, 10, 5))
+  expect_equal(undetected_prob(20, 10, list(mean=10, sd=5)),
+               undetected_prob(30, 20, list(mean=10, sd=5)))
 })
 
 test_that("undetected_prob increases in mean increase prob", {
-  expect_true(undetected_prob(20, 10, 10, 5) > undetected_prob(20, 10, 5, 5))
+  expect_true(undetected_prob(20, 10, list(mean=10, sd=5)) >
+                undetected_prob(20, 10, list(mean=5, sd=5)))
+})
+
+test_that("undetected_prob throws error if measurement before onset", {
+  expect_error(undetected_prob(9, 10, list(mean=10, sd=5)))
 })
 
 test_that("detected_after_unobserved_prob returns 0 or 1 as special cases", {
   # d2 = d1
-  expect_equal(detected_after_unobserved_prob(2, 2, 0, 10, 5), 0)
+  expect_equal(detected_after_unobserved_prob(2, 2, 0, list(mean=5, sd=5)), 0)
   # d2 = inf
-  expect_equal(detected_after_unobserved_prob(Inf, 2, 0, 10, 5), 1)
+  expect_equal(detected_after_unobserved_prob(Inf, 2, 0, list(mean=5, sd=5)), 1)
 })
 
 test_that("detected_after_unobserved_prob increases with latter observation
           time", {
   times <- seq(2, 10, 1)
   probs <- purrr::map_dbl(times,
-                          ~detected_after_unobserved_prob(., 2, 1, 10, 5))
+                          ~detected_after_unobserved_prob(., 2, 1,
+                                                          list(mean=5, sd=5)))
   expect_equal(sum(diff(probs) < 0), 0)
+})
+
+test_that("detected_after_unobserved_prob throws error if second measurement day
+          before first", {
+  expect_error(detected_after_unobserved_prob(3, 4, 0, list(mean=5, sd=5)))
+})
+
+test_that("observed_cases_single returns a number between 0 and I_remaining", {
+  ntries <- 20
+  count_fails <- 0
+  I_true <- 10
+  I_obs <- 2
+  I_remaining <- I_true - I_obs
+  for(i in 1:ntries) {
+    cases_obs <- observed_cases_single(I_obs, I_true, 20, 1, 0,
+                                       list(mean=10, sd=5))
+    if(cases_obs > I_remaining | cases_obs < 0)
+      count_fails <- count_fails + 1
+  }
+  expect_equal(count_fails, 0)
+})
+
+test_that("observed_cases_single throws error if observed cases exceeds true", {
+  expect_error(observed_cases_single(15, 13, 23, 22, 20, list(mean=10, sd=5)))
+})
+
+test_that("observed_cases_trajectory produces trajectories with given
+          characteristics", {
+  days_reporting <- seq(1, 10, 1)
+  true_cases <- 30
+  delay_parameters <- list(mean=1, sd=1)
+  day_onset <- 0
+  reported_cases <- observed_cases_trajectory(true_cases, days_reporting,
+                                              day_onset,
+                                              delay_parameters)
+  expect_equal(length(reported_cases), length(days_reporting))
+
+  nreps <- 20
+  count_increasing <- 0
+  count_max <- 0
+  for(i in 1:nreps) {
+    cases <- observed_cases_trajectory(true_cases, days_reporting, day_onset,
+                                       delay_parameters)
+    count_increasing <- count_increasing + dplyr::if_else(
+      sum(diff(cases) < 0) > 0, 1, 0)
+    count_max <- count_max + dplyr::if_else(max(cases) > true_cases, 1, 0)
+  }
+  expect_equal(count_increasing, 0)
+  expect_equal(count_max, 0)
+})
+
+test_that("observed_cases_trajectory increases faster with shorter delay", {
+  days_reporting <- seq(1, 10, 1)
+  true_cases <- 30
+  delay_parameters_fast <- list(mean=1, sd=1)
+  delay_parameters_slow <- list(mean=40, sd=1)
+  day_onset <- 0
+
+  reported_cases_fast <- observed_cases_trajectory(
+    true_cases, days_reporting, day_onset, delay_parameters_fast)
+  reported_cases_slow <- observed_cases_trajectory(
+    true_cases, days_reporting, day_onset, delay_parameters_slow)
+  expect_true(max(reported_cases_fast) > max(reported_cases_slow))
 })
