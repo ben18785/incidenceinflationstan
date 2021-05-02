048e5af507e72e4d8b07af308f491b1709d8c56f
ben18785 <ben.c.lambert@gmail.com>
2021-05-02 21:57:13 +0100

    Added tests for detection prob function

--- tests/testthat/test-simulated-data.R
+++ tests/testthat/test-simulated-data.R
@@ -2,3 +2,31 @@ test_that("undetected_prob returns correct probability", {
   expect_true(abs(undetected_prob(20, 10, 10, 5)
                   - 0.4334701) < 0.01)
 })
+
+test_that("undetected_prob approaches 0 as t1 approaches inf", {
+  expect_equal(undetected_prob(Inf, 10, 10, 5), 0)
+})
+
+test_that("undetected_prob only relative time differences matter", {
+  expect_equal(undetected_prob(20, 10, 10, 5),
+               undetected_prob(30, 20, 10, 5))
+})
+
+test_that("undetected_prob increases in mean increase prob", {
+  expect_true(undetected_prob(20, 10, 10, 5) > undetected_prob(20, 10, 5, 5))
+})
+
+test_that("detected_after_unobserved_prob returns 0 or 1 as special cases", {
+  # d2 = d1
+  expect_equal(detected_after_unobserved_prob(2, 2, 0, 10, 5), 0)
+  # d2 = inf
+  expect_equal(detected_after_unobserved_prob(Inf, 2, 0, 10, 5), 1)
+})
+
+test_that("detected_after_unobserved_prob increases with latter observation
+          time", {
+  times <- seq(2, 10, 1)
+  probs <- purrr::map_dbl(times,
+                          ~detected_after_unobserved_prob(., 2, 1, 10, 5))
+  expect_equal(sum(diff(probs) < 0), 0)
+})
