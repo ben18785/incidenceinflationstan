4b77bf8381e7f40218ba603efe11d2f7a7e2ebc3
ben18785 <ben.c.lambert@gmail.com>
2021-06-13 23:48:27 +0100

    added measles data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Influenza-like illness (ILI) in US from 2014-2015
 #'
-#' A dataset containing ILI case data with reporting delays specified
+#' A dataset containing ILI case data with reporting delay information
 #'
 #' @format A data frame with 729 rows and 4 variables:
 #' \describe{
@@ -11,3 +11,18 @@
 #' }
 #' @source \url{https://doi.org/10.1371/journal.pcbi.1007735}
 "ILI_2014"
+
+
+#' Measles outbreak data in Netherlands from 2013-2014
+#'
+#' A dataset containing measles case data with reporting delay information
+#'
+#' @format A data frame with 5934 rows and 4 variables:
+#' \describe{
+#'   \item{date_onset}{date when case onset}
+#'   \item{time_onset}{numeric time when case onset}
+#'   \item{time_reported}{time case reported}
+#'   \item{cases_reported}{number of cases reported}
+#' }
+#' @source \url{https://github.com/kassteele/Nowcasting}
+"measles_NL_2013"
