66b6488472f8d950285ebbe0eaa5f67771a3712b
ben18785 <ben.c.lambert@gmail.com>
2021-06-13 23:10:40 +0100

    added ILI data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Influenza-like illness (ILI) in US from 2014-2015
+#'
+#' A dataset containing ILI case data with reporting delays specified
+#'
+#' @format A data frame with 729 rows and 4 variables:
+#' \describe{
+#'   \item{date_onset}{date when case onset}
+#'   \item{time_onset}{numeric time when case onset}
+#'   \item{time_reported}{time case reported}
+#'   \item{cases_reported}{number of cases reported}
+#' }
+#' @source \url{https://doi.org/10.1371/journal.pcbi.1007735}
+"ILI_2014"
